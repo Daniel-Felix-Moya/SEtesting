4296e6577304ea6f742c8ba715141790951fe506
Daniel-Felix-Moya <daniel.felix@alu.uclm.es>
2025-09-24 10:43:04 +0200

    C6 - Rebasing

--- src/AppForSEII2526.API/People.cs
+++ src/AppForSEII2526.API/People.cs
@@ -2,11 +2,10 @@
 {
     public class People
     {
-<<<<<<< HEAD
         //this is a modification for C4
-=======
         //This is a comment for C3 in iss53
         //This is another comment but for C5
->>>>>>> iss53
+
+        //Comment for C6
     }
 }
